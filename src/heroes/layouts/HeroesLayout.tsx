2004e68588430a1dddb8215dff9d0d549e772315
Miguel Caballero <mc.caballero95@gmail.com>
2026-06-02 07:20:46 -0400

    adding lazy loading to SearchPage

--- src/heroes/layouts/HeroesLayout.tsx
+++ src/heroes/layouts/HeroesLayout.tsx
@@ -1,9 +1,25 @@
-import { Outlet } from "react-router"
+import { Link, Outlet } from "react-router"
 
 export const HeroesLayout = () => {
   return (
     <div className="bg-red-500">
-      <Outlet />
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/heroes/1">Hero</Link>
+        </li>
+        <li>
+          <Link to="/search">Busqueda</Link>
+        </li>
+        <li>
+          <Link to="/admin">Admin</Link>
+        </li>
+      </ul>
+      <section className="mt-10">
+        <Outlet />
+      </section>
     </div>
   )
 }
--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -1,10 +1,13 @@
+import { lazy } from "react";
+import { createBrowserRouter } from "react-router";
 import { AdminLayout } from "@/admin/layouts/AdminLayout";
 import { AdminPage } from "@/admin/pages/AdminPage";
 import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
 import { HeroPage } from "@/heroes/pages/hero/HeroPage";
 import { HomePage } from "@/heroes/pages/home/HomePage";
-import { SearchPage } from "@/heroes/pages/search/SearchPage";
-import { createBrowserRouter } from "react-router";
+
+// Lazy loading for SearchPage
+const SearchPage = lazy(() => import('@/heroes/pages/search/SearchPage'));
 
 export const appRouter = createBrowserRouter([
   {
