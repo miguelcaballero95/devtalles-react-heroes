e24975a00d19fc1f64672407ede6fb817c3b3e9a
Miguel Caballero <mc.caballero95@gmail.com>
2026-06-08 14:32:26 -0400

    deploying on github pages

--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -1,5 +1,5 @@
 import { lazy } from "react";
-import { createBrowserRouter, Navigate } from "react-router";
+import { createHashRouter, Navigate } from "react-router";
 import { AdminLayout } from "@/admin/layouts/AdminLayout";
 import { AdminPage } from "@/admin/pages/AdminPage";
 import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
@@ -7,9 +7,10 @@ import { HeroPage } from "@/heroes/pages/hero/HeroPage";
 import { HomePage } from "@/heroes/pages/home/HomePage";
 
 // Lazy loading for SearchPage
+// eslint-disable-next-line react-refresh/only-export-components
 const SearchPage = lazy(() => import('@/heroes/pages/search/SearchPage'));
 
-export const appRouter = createBrowserRouter([
+export const appRouter = createHashRouter([
   {
     path: '/',
     element: <HeroesLayout />,
